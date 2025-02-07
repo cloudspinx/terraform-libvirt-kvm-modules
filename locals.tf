86439f50b23811e746615c190f483b5a70cc6b1e
Josphat Mutai <josphatkmutai@gmail.com>
2025-02-07 22:40:16 +0300

    Use public key data in "Authorized Keys" format

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
   user_password_hash = var.set_user_password ? bcrypt(random_password.user_password[0].result) : null
 
   # SSH connection
-  generated_ssh_keys = var.generate_ssh_keys ? [trimspace(tls_private_key.ssh_key[0].public_key_pem)] : []
+  generated_ssh_keys = var.generate_ssh_keys ? [trimspace(tls_private_key.ssh_key[0].public_key_openssh)] : []
   combined_ssh_keys  = concat(var.ssh_keys, local.generated_ssh_keys)
   # vm_ip           = var.network_config == "" ? libvirt_domain.this_domain[0].network_interface.0.addresses[0] : ""
   # connection      = "ssh ${var.ssh_user_name}@${local.vm_ip}"
