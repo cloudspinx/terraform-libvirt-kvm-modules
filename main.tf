49196a624ea1344ce9142f72fe8e5185da7bfdf9
Josphat Mutai <josphatkmutai@gmail.com>
2025-02-08 17:13:00 +0300

    use network name

--- main.tf
+++ main.tf
@@ -172,8 +172,8 @@ resource "libvirt_domain" "this_domain" {
   cloudinit = element(libvirt_cloudinit_disk.commoninit[*].id, count.index)
 
   network_interface {
-    bridge         = var.bridge_name
-    # network_name   = "default"
+    # bridge         = var.bridge_name
+    network_name   = "default"
     wait_for_lease = true
     hostname       = format("${var.vm_hostname_prefix}%02d", count.index + var.index_start)
   }
@@ -240,24 +240,3 @@ resource "libvirt_domain" "this_domain" {
   #   }
   # }
 }
-
-resource "null_resource" "wait_for_ipv4" {
-  count = var.vm_count
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Waiting for IPv4 on ${libvirt_domain.this_domain[count.index].name}..."
-      while true; do
-        ip=$(virsh domifaddr ${libvirt_domain.this_domain[count.index].name} | awk '/ipv4/ {print $4}' | cut -d'/' -f1)
-        if [ -n "$ip" ]; then  # <-- POSIX-compliant syntax
-          echo "IPv4 found: $ip"
-          break
-        fi
-        sleep 5
-      done
-    EOT
-  }
-
-  depends_on = [libvirt_domain.this_domain]
-}
-
