d94a1a22836aff2713c87fa946ac42d6ac3c8394
Josphat Mutai <josphatkmutai@gmail.com>
2025-02-11 19:01:23 +0300

    output storage pool name

--- modules/storage-pool/outputs.tf
+++ modules/storage-pool/outputs.tf
@@ -2,3 +2,8 @@ output "pool_id" {
   description = "The ID of the created Libvirt storage pool"
   value       = try(libvirt_pool.storage[0].id, null)
 }
+
+output "name" {
+  description = "The name of the created Libvirt storage pool"
+  value       = try(libvirt_pool.storage[0].name, null)
+}
